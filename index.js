dda7ed7bc218b03babe0deb9a54d505f708bf08e
m.kavaleuskaya <gl_mkavaleuskaya>
2024-03-14 00:23:37 +0100

    Proxy 4

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 require('dotenv').config();
-const Koa = require('koa');
-const router = require('./routes.js');
+const express = require('express');
+const fetchAsteroids = require('./controller')
 
-const app = new Koa();
+const app = express();
 
-app.use(router.routes());
+app.get('/hi', async (req, res) => {
+    try {
+        const startDate = req.query.start??"2000-12-20";
+        const endDate = req.query.end??"2000-12-20";
+        const asteroids = await fetchAsteroids(startDate, endDate);
+        res.json(asteroids);
+    } catch (error) {
+        res.status(500);
+        res.send("Error fetching responce from NASA");
+    }
+});
+
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
 });
